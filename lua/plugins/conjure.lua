8795f7cd1436aedf9194fe998cb1f4c5c6f97ae3
Chris <lemoscvbl@gmail.com>
2025-01-23 19:44:38 -0300

    new stuff

---
+++ lua/plugins/conjure.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"Olical/conjure",
+		ft = { "clojure" },
+		lazy = true,
+		dependencies = { "PaterJason/cmp-conjure" },
+	},
+
+	{
+		"PaterJason/cmp-conjure",
+		config = function()
+			local cmp = require("cmp")
+			local config = cmp.get_config()
+			table.insert(config.sources, { name = "conjure" })
+			return cmp.setup(config)
+		end,
+	},
+}
